28a91caecd07b2e34263fc7ece75c236a7fa17e5
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-03-05 15:26:18 +0100

    add scaling to stars animation

--- three_pfl_2/animations/starsAnimation.js
+++ three_pfl_2/animations/starsAnimation.js
@@ -12,9 +12,13 @@ export function starsAnimation() {
     s.position.x = s.originalPosition.x * f;
     s.position.y = Math.abs(s.originalPosition.y * f);
     s.position.z = s.originalPosition.z * f;
+
+    s.scale.x = Math.abs(1 * f);
+    s.scale.y = Math.abs(1 * f);
+    s.scale.z = Math.abs(1 * f);
   });
   // depending on d, invert the direction
-  d ? worldState.starsState.distance += 0.07 : worldState.starsState.distance -= 0.07;
+  d ? worldState.starsState.distance += 0.08 : worldState.starsState.distance -= 0.08;
   // if you reach the limit (f = 1/100 distance), put distance back at the limit and change direction
   f < -1 && (d = !d, worldState.starsState.distance = -100);
   f > 1 && (d = !d, worldState.starsState.distance = 100);
