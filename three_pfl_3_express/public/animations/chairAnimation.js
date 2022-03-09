8ecaecfdfa3a82a93f872ce9b1b417717a8cf51d
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-03-09 13:03:15 +0100

    clear versions

--- three_pfl_3_express/public/animations/chairAnimation.js
+++
@@ -1,8 +0,0 @@
-import { chair } from '../main';
-
-export function chairAnimation() {
-  if (chair[0]) {
-    // console.log(chair) 
-    chair[0].rotation.x += 0.05;
-  }
-}
--- three_pfl_3_express/public/animations/icosahedronAnimation.js
+++
@@ -1,6 +0,0 @@
-import { icosahedron } from '../main';
-
-export function icosahedronAnimation() {
-  icosahedron && (icosahedron.rotation.y += 0.01);
-  icosahedron && (icosahedron.rotation.z += 0.007);
-}
